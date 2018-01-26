17215156cb5601f0aad265cdb7fa94d7367ce55a
Volodymyr Padovsky <vld06@ukr.net>
2018-01-26 19:10:54 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, { Component} from 'react';
+import Dropdown from './Dropdown';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Dropdown />
+      </div>
+    )
+  }
+}
+
+export default App;
